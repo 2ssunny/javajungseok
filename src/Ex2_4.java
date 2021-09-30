260cbb2a3fa42425441038cd52f22d641591082a
2sunny <eungseoni@gmail.com>
2021-09-30 20:52:48 +0900

    커밋테스트

---
+++ src/Ex2_4.java
@@ -0,0 +1,10 @@
+public class Ex2_4 {
+    public static void main(String[] args) {
+        var x=5;
+        System.out.println(x);
+
+        x=10;
+        System.out.println(x);
+    }
+    
+}
