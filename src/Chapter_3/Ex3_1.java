24279a1a3f14676fc89bb90b4b5077914bfaf014
2ssunny <eungseoni@gmail.com>
2021-10-03 17:22:12 +0900

    3_1

---
+++ src/Chapter_3/Ex3_1.java
@@ -0,0 +1,12 @@
+package Chapter_3;
+
+
+public class Ex3_1 {
+    public static void main(String[] args) {
+        int x, y;
+
+        x=y=3; //y에 3이 저장된 후 x에 3이 저장됨.
+        System.out.println("x="+y);
+        System.out.println("y="+y);
+    }
+}
