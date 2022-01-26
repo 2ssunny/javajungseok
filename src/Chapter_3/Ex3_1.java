492a6fd27704e6c899d090bd8d0f0220f3a40519
2ssunny <eungseoni@gmail.com>
2022-01-27 00:47:11 +0900

    test

--- src/Chapter_3/Ex3_1.java
+++ src/Chapter_3/Ex3_1.java
@@ -8,5 +8,6 @@ public class Ex3_1 {
         x=y=3; //y에 3이 저장된 후 x에 3이 저장됨.
         System.out.println("x="+y);
         System.out.println("y="+y);
+        
     }
 }
--- src/Chapter_3/Ex3_7.java
+++ src/Chapter_3/Ex3_7.java
@@ -2,6 +2,6 @@ package Chapter_3;
 
 public class Ex3_7 {
     public static void main(String[] args) {
-        
+        System.out.println("test");
     }
 }
