6c6890dff3a96c65e7e7dbe91c643381a25a8549
2ssunny <eungseoni@gmail.com>
2022-01-29 00:17:45 +0900

    3_7

---
+++ src/Test/Test.java
@@ -0,0 +1,11 @@
+package Test;
+
+public class Test {
+    public static void main(String[] args) {
+        byte a=10;
+        byte b=20;
+        byte c=a+b;
+
+        System.out.println(c);
+    }
+}
