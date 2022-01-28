d29daed28f26814adfae943f8161644b0d0bd198
2ssunny <eungseoni@gmail.com>
2022-01-29 00:23:59 +0900

    3_8

---
+++ src/Chapter_3/Ex3_8.java
@@ -0,0 +1,12 @@
+package Chapter_3;
+
+public class Ex3_8 {
+
+    public static void main(String[] args) {
+        byte a=10;
+        byte b=30;
+        int c=(int)(a*b);
+
+        System.out.println(c);
+    }
+}
--- src/Test/Test.java
+++ src/Test/Test.java
@@ -2,10 +2,6 @@ package Test;
 
 public class Test {
     public static void main(String[] args) {
-        byte a=10;
-        byte b=20;
-        byte c=a+b;
-
-        System.out.println(c);
+    
     }
 }
