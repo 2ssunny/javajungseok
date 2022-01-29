3dc22d6c246208f4a591512f284d1c5278a53f8c
2ssunny <eungseoni@gmail.com>
2022-01-30 00:21:29 +0900

    3_12

--- src/Chapter_3/Ex3_12.java
+++ src/Chapter_3/Ex3_12.java
@@ -5,7 +5,8 @@ public class Ex3_12 {
         int x=10;
         int y=8;
 
-        System.out.printf("%d를 $d로 나누면, %n", x, y);
+        System.out.printf("%d를 %d로 나누면, %n",x,y); //y가 %d로 나오는 오류 잡을것.
         System.out.printf("몫은 %d이고, 나머지는 %d입니다.",x/y,x%y);
+
     }
 }
