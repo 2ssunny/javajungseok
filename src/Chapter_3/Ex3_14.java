c82d2812d1d022d3549b287919a77606e6bf7473
2ssunny <eungseoni@gmail.com>
2022-02-03 00:34:07 +0900

    3_14-working

---
+++ src/Chapter_3/Ex3_14.java
@@ -0,0 +1,11 @@
+package Chapter_3;
+
+public class Ex3_14 {
+    public static void main(String[] args) {
+        String str1 = "abc";
+        String str2 = new String("abc");
+
+        System.out.printf("\"abc\"==\"abc\" ? %b%n", "abc"=="abc"); //앞에 abc는 문장, 뒤에 abc는 ==을 통해 trus or false값 출력
+        System.out.printf("str1==\"abc\" ? %b%n", str1=="abc");
+    }
+}
