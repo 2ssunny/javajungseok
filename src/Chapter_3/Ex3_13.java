02855076fd23a2e08372a40df4c2d2dbfb5732f2
2ssunny <eungseoni@gmail.com>
2022-02-02 23:52:21 +0900

    3_13

---
+++ src/Chapter_3/Ex3_13.java
@@ -0,0 +1,12 @@
+package Chapter_3;
+/**
+ * Ex3_13
+ */
+public class Ex3_13 {
+
+    public static void main(String[] args) {
+        System.out.println(-10%8);
+        System.out.println(10%-8);
+        System.out.println(-10%-8);
+    }
+}
